f0b0d081454b719da8d33f5e91d2ee049a77d75f
trongtoannguyen <trongtoan1609ht@gmail.com>
2024-09-02 20:24:07 +0700

    define spring boot structure organization

--- src/main/java/com/reddot/app/ReddotSpringApplication.java
+++ src/main/java/com/reddot/app/ReddotSpringApplication.java
@@ -1,4 +1,4 @@
-package com.reddot;
+package com.reddot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/reddot/app/controller/HelloController.java
+++ src/main/java/com/reddot/app/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.reddot.controller;
+package com.reddot.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
