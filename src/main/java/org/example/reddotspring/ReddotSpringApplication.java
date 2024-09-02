28e62d4ae7c10a0eec5adf87622de32e0091e7ad
trongtoannguyen <trongtoan1609ht@gmail.com>
2024-09-02 18:46:50 +0700

    init commit create Spring boot app

---
+++ src/main/java/org/example/reddotspring/ReddotSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.reddotspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReddotSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReddotSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/reddotspring/ReddotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reddotspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReddotSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
