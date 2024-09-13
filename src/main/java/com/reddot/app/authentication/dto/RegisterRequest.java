61b1a059a7edcccfae6b995f40215053907e7e85
trongtoannguyen <trongtoan1609ht@gmail.com>
2024-09-14 02:52:55 +0700

    split AuthRequest class to Login/Register and Validation enable easier to maintain

---
+++ src/main/java/com/reddot/app/authentication/dto/RegisterRequest.java
@@ -0,0 +1,34 @@
+package com.reddot.app.authentication.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NonNull;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+public class RegisterRequest {
+
+    @NonNull
+    @Size(min = 3, max = 30)
+    @Pattern(regexp = "^[a-zA-Z0-9_]{3,30}$")
+    private String username;
+    @NonNull
+    @Email
+    @Size(min = 3, max = 50)
+    @Pattern(regexp = "^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+.[a-zA-Z]{2,10}$",
+            message = "Invalid email format")
+    private String email;
+    @NonNull
+    @Size(min = 8,max = 30)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,30}$",
+            message = "Password must contain minimum 8 characters, at least one uppercase letter," +
+                    "one lowercase letter, one number and one special character")
+    private String password;
+
+    private Set<String> roles;
+}
