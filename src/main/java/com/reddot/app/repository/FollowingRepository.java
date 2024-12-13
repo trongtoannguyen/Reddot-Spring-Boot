4e9e277d25a8896fe46c46185dd436137a470113
bachxuan10 <bachxuan10@gmail.com>
2024-12-13 23:11:19 +0700

    calculate total number of followers

---
+++ src/main/java/com/reddot/app/repository/FollowingRepository.java
@@ -0,0 +1,10 @@
+package com.reddot.app.repository;
+
+import com.reddot.app.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface FollowingRepository extends JpaRepository<Follow, Integer> {
+    @Query("SELECT COUNT(f) FROM following f WHERE f.followed.id = :userId")
+    Long countFollowersByUserId( Integer userId);
+}
