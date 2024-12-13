0991d9846adcc70d75a8b0cc8d775d746d52211e
bachxuan10 <bachxuan10@gmail.com>
2024-12-13 23:14:17 +0700

    new repository

---
+++ src/main/java/com/reddot/app/repository/UserBadgeRepository.java
@@ -0,0 +1,33 @@
+package com.reddot.app.repository;
+
+import com.reddot.app.entity.Badge;
+import com.reddot.app.entity.UserBadge;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserBadgeRepository extends JpaRepository<UserBadge, Integer> {
+    @Query("SELECT COUNT(ub) FROM user_badges ub WHERE ub.user.id = :userId")
+    Long countBadgesByUserId(@NonNull Integer userId);
+
+    @Query("SELECT b FROM badges b " +
+            "JOIN user_badges ub ON b.id = ub.badge.id " +
+            "WHERE ub.user.id = :userId " +
+            "ORDER BY b.tier ASC")
+    List<Badge> findBadgesByUserId(@Param("userId") Integer userId);
+
+    // Kiểm tra xem người dùng đã nhận badge này chưa
+    boolean existsByUserIdAndBadgeId(Integer userId, Integer badgeId);
+
+
+
+    // Thêm một badge mới cho người dùng
+    UserBadge save(UserBadge userBadge);
+
+
+
+}
