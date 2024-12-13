3052aff6e7ba4fa464e9c4b706bd63ff548590a2
bachxuan10 <bachxuan10@gmail.com>
2024-12-13 23:14:58 +0700

    add more functions

--- src/main/java/com/reddot/app/repository/VoteRepository.java
+++ src/main/java/com/reddot/app/repository/VoteRepository.java
@@ -1,9 +1,30 @@
 package com.reddot.app.repository;
 
 import com.reddot.app.entity.Vote;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
+    // Tổng số upvotes cho các câu hỏi mà người dùng sở hữu
+    @Query("SELECT SUM(q.upvotes) FROM questions q WHERE q.user.id = :userId")
+    Long countUpvotesForQuestionsByUserId(@NonNull Integer userId);
+
+    // Tổng số downvotes cho các câu hỏi mà người dùng sở hữu
+    @Query("SELECT SUM(q.downvotes) FROM questions q WHERE q.user.id = :userId")
+    Long countDownvotesForQuestionsByUserId(@NonNull Integer userId);
+
+    // Đếm số upvotes cho các câu trả lời (comments) mà người dùng sở hữu
+    @Query("SELECT COUNT(v) FROM votes v " +
+            "JOIN v.comment c " +
+            "WHERE c.user.id = :userId AND v.voteType.id = 1")
+    Long countUpvotesForCommentsByUserId(@NonNull Integer userId);
+
+    // Đếm số downvotes cho các câu trả lời (comments) mà người dùng sở hữu
+    @Query("SELECT COUNT(v) FROM votes v " +
+            "JOIN v.comment c " +
+            "WHERE c.user.id = :userId AND v.voteType.id = 2")
+    Long countDownvotesForCommentsByUserId(@NonNull Integer userId);
 }
