692b6a03d919fc6933fbd41597007489deaa502f
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-01-09 06:19:39 +0800

    style: standardize quotation marks in CSS and JS files

--- Reddot-Client/src/stories/Button.jsx
+++ Reddot-Client/src/stories/Button.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import PropTypes from "prop-types";
 
-import PropTypes from 'prop-types';
-
-import './button.css';
+import "./button.css";
 
 /** Primary UI component for user interaction */
 export const Button = ({ primary, backgroundColor, size, label, ...props }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " ",
+      )}
       style={backgroundColor && { backgroundColor }}
       {...props}
     >
@@ -25,7 +27,7 @@ Button.propTypes = {
   /** What background color to use */
   backgroundColor: PropTypes.string,
   /** How large should the button be? */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
+  size: PropTypes.oneOf(["small", "medium", "large"]),
   /** Button contents */
   label: PropTypes.string.isRequired,
   /** Optional click handler */
@@ -35,6 +37,6 @@ Button.propTypes = {
 Button.defaultProps = {
   backgroundColor: null,
   primary: false,
-  size: 'medium',
+  size: "medium",
   onClick: undefined,
 };
