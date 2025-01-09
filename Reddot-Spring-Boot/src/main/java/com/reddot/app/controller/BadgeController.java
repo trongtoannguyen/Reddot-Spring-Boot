ca36e7a3ce7b82fce87d08ec9d6f2dee2657adc4
trongtoannguyen <trongtoan1609ht@gmail.com>
2025-01-09 16:45:25 +0800

    refactor: update API endpoint paths by removing the '/api' prefix

--- Reddot-Spring-Boot/src/main/java/com/reddot/app/controller/BadgeController.java
+++ Reddot-Spring-Boot/src/main/java/com/reddot/app/controller/BadgeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/badges")
+@RequestMapping("/badges")
 public class BadgeController {
 
     private final BadgeService badgeService;
--- Reddot-Spring-Boot/src/main/java/com/reddot/app/controller/ModerationController.java
+++ Reddot-Spring-Boot/src/main/java/com/reddot/app/controller/ModerationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/moderation")
+@RequestMapping("/moderation")
 @PreAuthorize("hasRole('MODERATOR') or hasRole('ADMIN')")
 public class ModerationController {
 
